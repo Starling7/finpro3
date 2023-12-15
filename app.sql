9f3750cd632c4a6ef9f986e11de221255177128d
Starling7 <129472418+Starling7@users.noreply.github.com>
2023-12-15 14:02:08 +0700

    Update app.sql

--- app.sql
+++ app.sql
@@ -1,26 +1,31 @@
-drop table if exists schedule;
-create table schedule (
+drop table if exists tickets;
+create table tickets (
 	id serial,
-	doctor_name text,
-	patient_name text,
+	tribune_name text,
+	supporter_name text,
 	gender text,
-	symptom text,
-	handphone text,
-	address text,
-	waktu time,
-	tanggal date
+	stadium_name text,
+	ticket_price text,
+	match_name text,
+	date_info date
 );
 
-insert into schedule (doctor_name, patient_name, gender, symptom, handphone, address, waktu, tanggal) 
+insert into tickets (tribune_name, supporter_name, gender, stadium_name, ticket_price, match_name, date_info) 
 values
-	('dr. Nurita', 'Ahmad Maulana', 'male', '["headache", "stomache"]', 62838, 'address1', '08:00', '2023-10-01'),
-	('dr. Yogi', 'Renata Zahab', 'female', '["cough", "flu"]', 62838, 'address2', '09:00', '2022-10-02'),
-	('dr. Wibowo', 'Nunuk Reni', 'female', '["cough", "flu"]', 62838, 'address3', '10:00', '2022-10-03'),
-	('dr. Yogi', 'Bro Ulil', 'male', '["cough", "headache"]', 62838, 'address4', '11:00', '2022-10-04'),
-	('dr. Ulama', 'Wah Bowi', 'male', '["headache", "flu"]', 62838, 'address5', '12:00', '2022-10-05'),
-	('dr. Ulama', 'Iis Mika', 'female', '["cough", "flu", "stomache", "headache"]', 62838, 'address6', '08:00', '2022-10-06'),
-	('dr. Ping', 'Zizah Lana', 'female', '["flu", "stomache", "headache"]', 62838, 'address7', '09:00', '2022-10-07'),
-	('dr. Nurita', 'Alif Iman', 'male', '["cough", "flu", "headache"]', 62838, 'address8', '10:00', '2022-10-08'),
-	('dr. Ping', 'Zaka Zaki', 'female', '["cough", "stomache", "headache"]', 62838, 'address9', '11:00', '2022-10-09'),
-	('dr. Wibowo', 'Faus Rahmi', 'male', '["cough"]', 62838, 'address10', '12:00', '2022-10-11')
+	('Economy', 'Agam', 'male', 'Jakarta International Stadium', 150000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('VVIP', 'Jose', 'male', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina',  '2024-10-03'),
+	('VVIP', 'Malik', 'male', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('VVIP', 'Yoze', 'female', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('Economy', 'Ahmad', 'male', 'Jakarta International Stadium', 150000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('Economy', 'Agam', 'male', 'Jakarta International Stadium', 150000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('VVIP', 'Jose', 'male', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina',  '2024-10-03'),
+	('VVIP', 'Malik', 'male', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('VVIP', 'Yoze', 'female', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('Economy', 'Agam', 'male', 'Jakarta International Stadium', 150000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('VVIP', 'Jose', 'male', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina',  '2024-10-03'),
+	('VVIP', 'Malik', 'male', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('VVIP', 'Yoze', 'female', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('Economy', 'Ahmad', 'male', 'Jakarta International Stadium', 150000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('Economy', 'Ahmad', 'male', 'Jakarta International Stadium', 150000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('Regular', 'Ammar', 'male', 'Jakarta International Stadium', 250000 , 'Indonesia vs Argentina', '2024-10-03')
 	;
