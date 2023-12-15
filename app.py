5d1b9637fdc665b78860d8b63202e275fa9eeb89
Starling7 <129472418+Starling7@users.noreply.github.com>
2023-12-15 14:59:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,13 @@ list_tribune = ['', 'Economy', 'Regular', 'VIP', 'VVIP']
 list_gender = ['', 'male', 'female']
 list_match= ['', 'Indonesia vs Argentina', 'Indonesia vs Thailand', 'Indonesia vs Malaysia']
 list_price = ['', '150000', '250000', '350000', '500000']
-
+list_stadion = ['','Jakarta International Stadium', 'Jatidiri', 'Maguwoharjo'
+                
 conn = st.connection("postgresql", type="sql", 
                      url="postgresql://radityacr740:o8KrhDcWj4wN@ep-super-smoke-81752083.us-east-2.aws.neon.tech/fpmbddb")
 with conn.session as session:
     query = text('CREATE TABLE IF NOT EXISTS TICKETS (id serial, tribune_name varchar, supporter_name varchar, gender char(25), \
-                                                       stadium_name varchar, ticket_price varchar, match_name varchar, date_info date);')
+                                                       stadion varchar, ticket_price varchar, match_name varchar, date_info date);')
     session.execute(query)
 
 st.header('FOOTBALL TICKETS DATABASES')
@@ -23,7 +24,7 @@ if page == "View Data":
 if page == "Edit Data":
     if st.button('Tambah Data'):
         with conn.session as session:
-            query = text('INSERT INTO tickets (tribune_name, supporter_name, gender, stadium_name, ticket_price, match_name, date_info) \
+            query = text('INSERT INTO tickets (tribune_name, supporter_name, gender, stadion, ticket_price, match_name, date_info) \
                           VALUES (:1, :2, :3, :4, :5, :6, :7);')
             session.execute(query, {'1':'', '2':'', '3':'', '4':'[]', '5':'', '6':'', '7':None})
             session.commit()
@@ -34,7 +35,7 @@ if page == "Edit Data":
         tribune_name_lama = result["tribune_name"]
         supporter_name_lama = result["supporter_name"]
         gender_lama = result["gender"]
-        stadium_name_lama = result["stadium_name"]
+        stadion_lama = result["stadion"]
         ticket_price_lama = result["ticket_price"]
         match_name_lama = result["match_name"]
         date_info_lama = result["date_info"]
@@ -45,6 +46,7 @@ if page == "Edit Data":
                 supporter_name_baru = st.text_input("supporter_name", supporter_name_lama)
                 gender_baru = st.selectbox("gender", list_gender, list_gender.index(gender_lama))
                 ticket_price_baru = st.selectbox("ticket_price", list_price, list_price.index(ticket_price_lama))
+                stadion_baru = st.selectbox("stadion", list_stadion, list_stadion.index(stadion_lama))
                 match_name_baru = st.selectbox("match_name", list_match, list_match.index(match_name_lama))
                 date_info_baru = st.date_input("date_info", date_info_lama)
                 
@@ -54,10 +56,10 @@ if page == "Edit Data":
                     if st.form_submit_button('UPDATE'):
                         with conn.session as session:
                             query = text('UPDATE tickets \
-                                          SET tribune_name=:1, supporter_name=:2, gender=:3, stadium_name=:4, \
+                                          SET tribune_name=:1, supporter_name=:2, gender=:3, stadion=:4, \
                                           ticket_price=:5, match_name=:6, date_info=:7 \
                                           WHERE id=:8;')
-                            session.execute(query, {'1':tribune_name_baru, '2':supporter_name_baru, '3':gender_baru, '4':str(stadium_name_baru), 
+                            session.execute(query, {'1':tribune_name_baru, '2':supporter_name_baru, '3':gender_baru, '4':str(stadion_baru), 
                                                     '5':ticket_price_baru, '6':match_name_baru, '7':date_info_baru, '8':id})
                             session.commit()
                             st.experimental_rerun()
