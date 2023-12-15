c7c77b74ffbc2211e96a95e41839c5c866efd1f4
Starling7 <129472418+Starling7@users.noreply.github.com>
2023-12-15 15:05:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from sqlalchemy import text
 
 list_tribune = ['', 'Economy', 'Regular', 'VIP', 'VVIP']
 list_gender = ['', 'male', 'female']
-list_match= ['', 'Indonesia vs Argentina', 'Indonesia vs Thailand', 'Indonesia vs Malaysia']
-list_price = ['', '150000', '250000', '350000', '500000']
 list_stadion = ['','Jakarta International Stadium', 'Jatidiri', 'Maguwoharjo']
-                
+list_price = ['', '150000', '250000', '350000', '500000']
+list_match= ['', 'Indonesia vs Argentina', 'Indonesia vs Thailand', 'Indonesia vs Malaysia']
+
 conn = st.connection("postgresql", type="sql", url="postgresql://radityacr740:o8KrhDcWj4wN@ep-super-smoke-81752083.us-east-2.aws.neon.tech/fpmbddb")
 with conn.session as session:
     query = text('CREATE TABLE IF NOT EXISTS TICKETS (id serial, tribune_name varchar, supporter_name varchar, gender char(25), \
